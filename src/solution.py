db476daa30c9b9832bdf55c042e6d8db88268a66
Noey Ignacio <hello@noeyislearning.dev>
2023-09-14 21:21:59 +0800

    refactor: Update `solution.py`

--- src/solution.py
+++ src/solution.py
@@ -1,5 +1,2 @@
 def calculate_factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * calculate_factorial(n - 1)
+    pass # Remove the function "pass" and write your code here
