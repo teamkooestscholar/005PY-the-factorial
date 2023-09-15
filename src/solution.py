b86435120c2e2582b05e16e7c5f9535021a9352f
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-15 18:23:03 +0800

    added solution from pycharm and added  co author
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,21 @@
-def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    if n == 0:
+        return "1"
+    #Put the factor number in a array
+    factors = []
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+        factors.append(str(i))
+
+    #using slicing notation creates new list  but its in reverse
+    factors = factors[::-1]
+    factorial_expression = f"The factorial of {n} is {' x '.join(factors)} = {result}"
+    return factorial_expression
+
+
+number = int(input("Enter a number to give factorial: "))
+result = factorial(number)
+print(result)
