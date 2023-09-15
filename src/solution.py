5d319709e6636ee1e272d6c6bd9eacce738c30d4
rzzel <ruzzdabid@gmail.com>
2023-09-15 22:14:35 +0800

    Update solution.py
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,4 +1,6 @@
 def calculate_factorial(n):
     # Remove the function "pass" and write your code here
-    
-    return n * calculate_factorial(n - 1)
+      if n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
