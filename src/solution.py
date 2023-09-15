8491ede10bb5e5ce6d4a405f872f43dc48d738ac
Minato Arisato <123571129+ZackFair-5698@users.noreply.github.com>
2023-09-15 17:12:40 +0800

    calculate_factorial

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,14 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n == 0:
+        return 1
+
+    factorial = 1
+    while n > 0:
+        factorial *= n
+        n -= 1
+
+    return factorial
+
+# Test cases
+print(calculate_factorial(5))  # Output: 120
+print(calculate_factorial(10))  # Output: 3628800
