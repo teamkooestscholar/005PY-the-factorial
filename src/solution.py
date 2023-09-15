cf8431cd52fa801493168a24f013aa246f7319f8
Cy12swe <andreisagum@gmail.com>
2023-09-15 23:43:40 +0800

    Update solution.py
    Created a function to calculate the factorial of a number without using loops and the math import.
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,5 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    return 1 if n <= 1 else n * calculate_factorial(n - 1)
+
+result = calculate_factorial(0)
+print(result)
