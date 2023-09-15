782df1ef859dffd15b32292ff31f63d70e4a28a7
jlfx09 <ykiritsu@gmail.com>
2023-09-15 18:39:51 +0800

    Updated Solution.py
    Removed pass from line 2 and added the intended code past line 2 for calculating the factorial of a given number.
    
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,20 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    def factorial(acc,k):
+        if k == 0:
+            return acc
+        else:
+            return factorial(acc*k, k-1)
+        
+    if n < 0:
+        raise ValueError("Negative numbers are undefined for factorials.")
+    else:
+        return factorial(1,n)
+    
+    #testing
+try:
+    n = int(input("Enter an Integer that is not negative."))
+    result = calculate_factorial(n)
+    print(f"The factorial of {n} is {result}")
+except ValueError as e:
+    print(e)
+    
