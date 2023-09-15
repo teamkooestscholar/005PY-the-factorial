6591e8cb01349734d24354c7b9416b2cebc4d126
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-09-15 22:01:13 +0800

    solution

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,28 @@
-def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+# Description: Write a function that calculates and returns the factorial of a non-negative integer.
+
+# Example #1
+# Input: `calculate_factorial(5)`
+# Output: `120`
+# Explanation: The factorial of 5 is 5 x 4 x 3 x 2 x 1 = 120.
+
+# Example #2
+# Input: `calculate_factorial(10)`
+# Output: `3628800`
+# Explanation: The factorial of 10 is 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = 3628800.
+
+# Constraints
+# Do not use the built-in math.factorial() function.
+# Do not use recursion.
+# Do not use any loops.
+
+from functools import reduce
+
+def factorial(n):
+    """ Return the factorial of a number """
+    if n < 0:
+        raise ValueError("Factorial is Only defined for non-negative integers")
+    return reduce(lambda x, y: x * y, range(1, n + 1), 1)
+
+user_num = int(input("Enter a number that you want to get its factorial value: "))
+print(f"The factorial of {user_num} is {factorial(user_num)}")
+
