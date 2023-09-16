58ad61a34376976dee29ceb7a244c37b8b5fcec6
deguzmankaira <deguzmankairag@gmail.com>
2023-09-16 22:13:35 +0800

    005PY

--- src/solution.py
+++ src/solution.py
@@ -1,5 +1,10 @@
 def calculate_factorial(n):
     if n == 0:
         return 1
-    else:
-        return n * calculate_factorial(n-1)
+    
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    
+    return factorial
+
