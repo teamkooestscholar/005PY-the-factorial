805cb21265c2db3436b77bc275575f2763a4cdb7
Ecniov <vincedeato@gmail.com>
2023-09-16 23:14:54 +0800

    update-05
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,13 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        while n > 1:
+            result *= n
+            n -= 1
+        return result
+
+# Example usage:
+n = 5
+print(calculate_factorial(n))  # Output: 120
