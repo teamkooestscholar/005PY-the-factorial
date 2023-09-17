c5ce4357927b7b0947dbd3558b546f583886da9c
gemoftruesight <kcdsanchezz@gmail.com>
2023-09-17 16:26:11 +0800

    Co-authored by:noeyislearning<hello@noeyislearning.dev>
    Co-authored by:noeyislearning<hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,8 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
