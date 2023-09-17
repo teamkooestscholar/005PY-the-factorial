d00d82d56040d954b79e699557842edb51f17c52
Prgrmmr633 <airesmacalino09@gmail.com>
2023-09-17 15:42:16 +0800

    Co-authored-by: Noey Ignacio <hello@noeyislearning.dev> Co-authored-by: Noey Ignacio <noeyislearning@gmail.com>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,13 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n < 0:
+        return None  
+    elif n == 0:
+        return 1  
+    else:
+        return n * calculate_factorial(n - 1)
+    
+factorial5 = calculate_factorial(5)
+factorial10 = calculate_factorial(10)
+
+print("The factorial of 5 is: ", factorial5)
+print("The factorial of 10 is: ", factorial10)
