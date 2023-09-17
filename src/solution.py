73df7a18910359f962697a01a75df8a7999ad3b1
Raroman04 <raroman@student.hau.edu.ph>
2023-09-17 23:15:00 +0800

    Update solution.py

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,14 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
+
+result1 = calculate_factorial(5)
+print(f"Ans: {result1}")
+print(f"The factorial of 5 is 5 x 4 x 3 x 2 x 1 = {result1}")
+print()
+
+result2 = calculate_factorial(10)
+print(f"Ans: {result2}")
+print(f"The factorial of 10 is 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = {result2}")
