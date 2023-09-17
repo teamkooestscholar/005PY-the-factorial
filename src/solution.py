55c5a3a072b691b4df62fba0b5c1e9eb9fd78ea3
Jonash Gania <jonashtiglao@gmail.com>
2023-09-17 20:45:48 +0800

    Calculating factorial
    Co-authored-by: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,9 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if(n < 0):
+        return "Error: n must be >= 0"
+    elif(n == 0):
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
+
+calculate_factorial(5)
