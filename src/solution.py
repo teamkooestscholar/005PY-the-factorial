550df510cb2622488eae06e9cf448b53f2b9c765
Raroman04 <raroman@student.hau.edu.ph>
2023-09-17 23:24:10 +0800

    Added Solution
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -9,6 +9,7 @@ print(f"Ans: {result1}")
 print(f"The factorial of 5 is 5 x 4 x 3 x 2 x 1 = {result1}")
 print()
 
+
 result2 = calculate_factorial(10)
 print(f"Ans: {result2}")
 print(f"The factorial of 10 is 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = {result2}")
