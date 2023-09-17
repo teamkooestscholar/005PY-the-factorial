67bb3d88aa8df8064b9132e9bef7f1b2676e458b
ChristianPaulo24 <123344269+ChristianPaulo24@users.noreply.github.com>
2023-09-17 14:38:31 +0800

    solution.py

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,19 @@
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Example usages
+result1 = calculate_factorial(5)
+result2 = calculate_factorial(0)
+result3 = calculate_factorial(-3)
+
+print("Factorial of 5:", result1)  # This will print "Factorial of 5: 120"
+print("Factorial of 0:", result2)  # This will print "Factorial of 0: 1"
+print("Factorial of -3:", result3)  # This will print "Factorial is not defined for negative numbers"
