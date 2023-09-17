49130061c07808c307fb48fbb3d25da88cddd266
Jayem0709 <123857306+Jayem0709@users.noreply.github.com>
2023-09-17 11:05:18 +0000

    Solution
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,11 @@
+# def calculate_factorial(n):
+#     pass # Remove the function "pass" and write your code here
+
+
+
 def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+    if n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
+
--- src/test.py
+++ src/test.py
@@ -5,14 +5,17 @@ class TestCalculateFactorial(unittest.TestCase):
     def test_factorial_of_zero(self):
         result = calculate_factorial(0)
         self.assertEqual(result, 1)
+        print("The factorial of n is ", result)
 
     def test_factorial_of_positive_number(self):
         result = calculate_factorial(5)
         self.assertEqual(result, 120)
+        print("The factorial of n is ", result)
 
     def test_factorial_of_large_number(self):
         result = calculate_factorial(10)
         self.assertEqual(result, 3628800)
+        print("The factorial of n is ", result)
 
 if __name__ == '__main__':
     unittest.main()
