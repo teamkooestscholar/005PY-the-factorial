49ff9a2218f4d00e2a0b6af814de92c7ecd58695
K-A-Elardo <142892729+K-A-Elardo@users.noreply.github.com>
2023-09-21 19:10:02 +0800

    yes

--- src/solution.py
+++ src/solution.py
@@ -1,2 +1,10 @@
-def calculate_factorial(n):
-    pass # Remove the function "pass" and write your code here
+from functools import reduce
+
+def factorial(n):
+    """ Return the factorial of a number """
+    if n < 0:
+        raise ValueError("Factorial is Only defined for non-negative integers")
+    return reduce(lambda x, y: x * y, range(1, n + 1), 1)
+
+user_num = int(input("Enter a number that you want to get its factorial value: "))
+print(f"The factorial of {user_num} is {factorial(user_num)}")
